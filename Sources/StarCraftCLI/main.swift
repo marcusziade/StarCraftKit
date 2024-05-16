4aecc4c2c866c0211ed803c0a8e89e71ed87a28d
Marcus Ziadé <guitaripod@icloud.com>
2024-05-15 19:20:05 -0700

    Add more output fields to upcomingTournaments CLI feature

--- Sources/StarCraftCLI/main.swift
+++ Sources/StarCraftCLI/main.swift
@@ -107,31 +107,57 @@ func ongoingTournaments() async {
 func upcomingTournaments() async {
     do {
         let response = try await api.allTournaments()
-        let upcoming = response.tournaments.filter { $0.beginAt > Date() }  // Filtering upcoming tournaments
+        let upcoming = response.upcomingTournaments
 
-        // Format and print all props for each tournament
         let formattedTournaments = upcoming.map { tournament in
-            """
-            Name: \t\t\t\(tournament.name)
-            Start Date: \t\t\(DateFormatter.prettyFormatter.string(from: tournament.beginAt))
-            End Date: \t\t\(DateFormatter.prettyFormatter.string(from: tournament.endAt))
-            League: \t\t\(tournament.league?.name ?? "Unknown")
-            Game: \t\t\t\(tournament.videogame?.name ?? "Unknown")
-            Tier: \t\t\t\(tournament.tier)
-            Slug: \t\t\t\(tournament.slug)
-            Has Bracket: \t\t\(tournament.hasBracket)
-            Live Supported: \t\(tournament.liveSupported)
-            Detailed Stats: \t\(tournament.detailedStats)
-            Prize Pool: \t\t\(tournament.prizepool ?? "Unknown")
-            Series: \t\t\(tournament.serie?.name ?? "Unknown")
-            Winner ID: \t\t\(tournament.winnerId ?? 0)
-            Winner Type: \t\t\(tournament.winnerType ?? "Unknown")
-            Match Count: \t\t\(tournament.matches?.count ?? 0)
-            Last Modified: \t\t\(DateFormatter.prettyFormatter.string(from: tournament.modifiedAt))
-            ID: \t\t\t\(tournament.id)
-            League ID: \t\t\(tournament.leagueId)
-            Series ID: \t\t\(tournament.serieId)
-            Game Slug: \t\t\(tournament.videogame?.slug ?? "Unknown")
+            let matchesInfo = tournament.matches?.map { match in
+                """
+                Match Name:      \(match.name)
+                Match ID:        \(match.id ?? 0)
+                Status:          \(match.status)
+                Scheduled At:    \(match.scheduledAt.map { DateFormatter.prettyFormatter.string(from: $0) } ?? "Unknown")
+                Begin At:        \(match.beginAt.map { DateFormatter.prettyFormatter.string(from: $0) } ?? "Unknown")
+                End At:          \(match.endAt.map { DateFormatter.prettyFormatter.string(from: $0) } ?? "Unknown")
+                Number of Games: \(match.numberOfGames)
+                Match Type:      \(match.matchType)
+                Detailed Stats:  \(match.detailedStats)
+                Live Supported:  \(match.live.supported)
+                Draw:            \(match.draw)
+                Forfeit:         \(match.forfeit)
+                Rescheduled:     \(match.rescheduled)
+                Game Advantage:  \(match.gameAdvantage ?? 0)
+                Winner ID:       \(match.winnerId ?? 0)
+                Winner Type:     \(match.winnerType ?? "Unknown")
+                Opponents:       \(match.opponents?.map { "\($0.opponent.name) (\($0.type))" }.joined(separator: ", ") ?? "Unknown")
+                Streams:         \(match.streamsList.compactMap(\.embedUrl))
+                \n
+                """
+            }.joined(separator: "\n\n") ?? "No Matches"
+
+            return """
+            Name:             \(tournament.name)
+            Start Date:       \(DateFormatter.prettyFormatter.string(from: tournament.beginAt))
+            End Date:         \(DateFormatter.prettyFormatter.string(from: tournament.endAt))
+            League:           \(tournament.league?.name ?? "Unknown")
+            League Image:     \(tournament.league?.imageUrl?.absoluteString ?? "–")
+            League URL:       \(tournament.league?.url?.absoluteString ?? "–")
+            League ID:        \(tournament.leagueId)
+            Game:             \(tournament.videogame?.name ?? "Unknown")
+            Game Slug:        \(tournament.videogame?.slug ?? "Unknown")
+            Tier:             \(tournament.tier)
+            Slug:             \(tournament.slug)
+            Has Bracket:      \(tournament.hasBracket)
+            Live Supported:   \(tournament.liveSupported)
+            Detailed Stats:   \(tournament.detailedStats)
+            Prize Pool:       \(tournament.prizepool ?? "Unknown")
+            Series:           \(tournament.serie?.name ?? "Unknown")
+            Series ID:        \(tournament.serieId)
+            Winner ID:        \(tournament.winnerId ?? 0)
+            Winner Type:      \(tournament.winnerType ?? "Unknown")
+            Match Count:      \(tournament.matches?.count ?? 0)
+            Matches:          \(matchesInfo)
+            Last Modified:    \(DateFormatter.prettyFormatter.string(from: tournament.modifiedAt))
+            ID:               \(tournament.id)
             \n
             """
         }
